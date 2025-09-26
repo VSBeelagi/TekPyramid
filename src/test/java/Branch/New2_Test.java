df4fd26ad848a9c035cabd1f9a81d0de4af36629
VSBeelagi <101255076+VSBeelagi@users.noreply.github.com>
2025-09-26 14:36:24 +0530

    Update New2_Test.java

--- src/test/java/Branch/New2_Test.java
+++ src/test/java/Branch/New2_Test.java
@@ -4,13 +4,19 @@ public class New2_Test {
 
 	public static void main(String[] args) {
 		
-		
 		System.out.println("-- new -2 ---");
 
 		
 		
 		
+
 		
+		
+		
+		
+
+		System.out.println("-- new -3 ---");
+
 	}
 
 }
